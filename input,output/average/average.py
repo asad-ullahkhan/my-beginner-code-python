6d8c00eb3d3eebae7c30904aa8a65c5bd4dc701f
asad_ullah_khan <au29072005@gmail.com>
2024-06-26 20:35:13 +0500

    Add files via upload

---
+++ input,output/average/average.py
@@ -0,0 +1,4 @@
+val1=float(input("enter first value ",))
+val2=float(input("enter second value ",))
+average=(val1+val2)/2
+print("the average of",val1,"and",val2,"is",average)
---
+++ input,output/equal/equal.py
@@ -0,0 +1,3 @@
+val1=int(input("enter your first value: ",))
+val2=int(input("enter your second value: ",))
+print("is",val1,"equal to",val2,":",val1==val2)
