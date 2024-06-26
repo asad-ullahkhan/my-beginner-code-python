ae3a19e60fafdca866acb23e531e4b320aab9e30
asad_ullah_khan <au29072005@gmail.com>
2024-06-26 20:29:43 +0500

    Add files via upload

---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+no=float(input("enter your number: ",))
+if no%2==0:
+    print(no,"is even.")
+elif no%2==1:
+    print(no,"is odd.")
+else:
+    print("you have entered a wrong input")        
---
+++ occurance_of_specific.py
@@ -0,0 +1,3 @@
+line=input("Enter a line: ",)
+find=input("what do you want to find: ",)
+print("the number of occurance of",find,"is",line.count(find))
